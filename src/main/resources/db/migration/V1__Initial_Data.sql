1c34f4c4615dd00dff294936fb421b8d5623cfdd
fsngpz <fsangap18@gmail.com>
2024-11-18 15:02:17 +0700

    add missing enum and default data for catalogs

---
+++ src/main/resources/db/migration/V1__Initial_Data.sql
@@ -0,0 +1,7 @@
+/*
+ * Insert Default Catalog
+ */
+INSERT INTO catalogs (service_type, description, price, creator_id, updater_id)
+VALUES ('DEEP_CLEANING', 'Deep Cleaning', '75000', 'SYSTEM', 'SYSTEM'),
+       ('RAPID_CLEANING', 'Rapid Cleaning', '45000', 'SYSTEM', 'SYSTEM'),
+       ('REPAIR', 'Repair', '150000', 'SYSTEM', 'SYSTEM');
